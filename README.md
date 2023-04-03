3a01dfd41c4021f8957448761b19e9972cd7ba61
omar12345-ons <123997339+omar12345-ons@users.noreply.github.com>
2023-04-03 02:57:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-ze1n8a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-jlchwf)
---
+++ index.ts
@@ -0,0 +1,10 @@
+let num1,num2,num3,num4,num5,num6,num7;
+num1 = porsIent(prompt(ادخل عدد اوراق النقود));
+num2 = porsIent(prompt(ادخل فئه النقود));
+num3 =porsIent(prompt(ادخل عدد اوراق النقود));
+num4 = porsIent(prompt(ادخل فئه النقود));
+num5 = num1 * num2
+num6 = num3 * num4
+num7 = num6 + num5
+
+console log.(num7);
